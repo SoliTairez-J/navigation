2593f1ac54a1f76fe235470f5700c00d18dfabd3
Warawit Siriyong <warawit.j@gmail.com>
2021-11-13 03:00:15 +0700

    finish commit

---
+++ src/components/page/Member.js
@@ -0,0 +1,11 @@
+const Member = ()=>{
+    return (
+        <div>
+            <h1>สมาชิก</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consequat dui nec orci posuere, in blandit arcu posuere. Donec tincidunt interdum erat nec efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus venenatis convallis ipsum, vel commodo mauris porta sed. Sed hendrerit porta nibh ut bibendum. Aenean venenatis dui a condimentum ultricies. Ut consequat lorem lorem, sit amet feugiat lacus pulvinar vel. Praesent venenatis at turpis at faucibus.</p>
+            <button>สมัครสมาชิก</button>
+        </div>
+    )
+}
+
+export default Member
---
+++ src/components/page/Product.js
@@ -0,0 +1,11 @@
+const Product = ()=>{
+    return (
+        <div>
+            <h1>สินค้า</h1>
+            <p>Etiam pharetra risus sit amet est cursus pretium. Quisque sit amet finibus mauris, at mattis nulla. Cras vel mauris felis. Quisque venenatis diam non condimentum pulvinar. Nunc viverra dictum elit, eget ullamcorper ante ullamcorper quis. Maecenas id neque urna. Aliquam et porttitor nisi, ut pulvinar eros. Ut massa erat, ullamcorper vitae maximus et, lacinia eget ante. Mauris ultrices magna turpis, eu scelerisque urna aliquam at. Nam mattis ipsum mattis blandit volutpat. Nunc vel odio vel nisi euismod tincidunt. Nunc pellentesque lectus suscipit turpis tincidunt, nec iaculis mauris placerat. Morbi convallis pretium dolor et lacinia. Etiam venenatis at lacus quis hendrerit. Sed et ullamcorper nibh, vitae euismod lorem.</p>
+            <button>ซื้อสินค้า</button>
+        </div>
+    )
+}
+
+export default Product
